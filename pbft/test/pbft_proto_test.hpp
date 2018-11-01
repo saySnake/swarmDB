2cc7a8cf5f999d231331eeed9d5dad1907d8bb6b
Paul Archard <paul@bluzelle.com>
2018-11-01 10:56:19 -0700

    KEP-613: moved test fixture class into header file

---
+++ pbft/test/pbft_proto_test.hpp
@@ -0,0 +1,50 @@
+// Copyright (C) 2018 Bluzelle
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License, version 3,
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+
+#include <pbft/test/pbft_test_common.hpp>
+
+using namespace ::testing;
+
+namespace bzn
+{
+    using namespace test;
+
+    class pbft_proto_test : public pbft_test
+    {
+    public:
+        // send a fake request to SUT
+        std::shared_ptr<pbft_operation> send_request();
+
+        // send a preprepare message to SUT
+        void send_preprepare(std::shared_ptr<pbft_operation> operation);
+
+        // send fake prepares from all nodes to SUT
+        void send_prepares(std::shared_ptr<pbft_operation> operation);
+
+        // send fake commits from all nodes to SUT
+        void send_commits(std::shared_ptr<pbft_operation> operation);
+
+        void prepare_for_checkpoint(size_t seq);
+
+        void force_checkpoint(size_t seq);
+
+        void run_transaction_through_primary(bool commit = true);
+
+        void run_transaction_through_backup(bool commit = true);
+
+    private:
+        size_t index = 0;
+        uint64_t view = 1;
+    };
+}
+
